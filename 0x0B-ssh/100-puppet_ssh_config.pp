ea0d527be12f029616387478b71f2fe64d824e9d
EimanEltayeb <em_moh092@hotmail.com>
2024-03-24 17:16:09 -0700

    f

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Client configuration file (w/ Puppet)
 file { '~/.ssh/config':
   ensure  => present,
-  content => "Host ubuntu@100.25.162.100 \n IdentityFile ~/.ssh/school \n PasswordAuthentication no",
+  content => "Host ubuntu@100.25.162.100 \nIdentityFile ~/.ssh/school \nPasswordAuthentication no",
 }
