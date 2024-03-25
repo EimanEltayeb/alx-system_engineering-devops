7ceeb0e9170db117b66e0b1846f34b5a0b2722be
EimanEltayeb <em_moh092@hotmail.com>
2024-03-24 17:45:42 -0700

    gfdg

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,5 +1,5 @@
 # Client configuration file (w/ Puppet)
 exec { 'ssh':
-  command  => '/bin/ssh -o PasswordAuthentication no',
+  command  => '/bin/ssh -i ~/.ssh/school ubuntu@100.25.162.100 -o PasswordAuthentication no',
   provider => 'shell',
 }
